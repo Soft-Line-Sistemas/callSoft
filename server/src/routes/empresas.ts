e1ee64f86b259be898a8854df9d6633717137dca
alandas71 <alanalves.dev@gmail.com>
2025-12-16 08:52:51 -0300

    Fix: removido a pasta server

--- server/src/routes/empresas.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { EmpresasController } from "../controllers/empresasController";
-
-const router = Router();
-const controller = new EmpresasController();
-
-router.post("/", (req, res) => controller.create(req, res));
-
-export default router;
